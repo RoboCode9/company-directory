53e932ee45505236e82457969d11905c5bd20602
RoboCode9 <ingresshelps@gmail.com>
2024-02-04 16:06:28 -0600

    card details done

--- src/components/MainCardSingle.vue
+++ src/components/MainCardSingle.vue
@@ -1,8 +1,9 @@
 <script setup>
 import { faker } from '@faker-js/faker'
-const fullname = faker.person.fullName()
+
 const firstname = faker.person.firstName()
 const lastname = faker.person.lastName()
+const fullname = faker.person.fullName({firstName: firstname, lastName: lastname})
 const job_title = faker.person.jobTitle()
 const bio = faker.lorem.lines(2)
 const username = faker.internet.userName({firstName: firstname, lastName: lastname}).toLowerCase()
@@ -11,12 +12,14 @@ const username = faker.internet.userName({firstName: firstname, lastName: lastna
 
 <template>
     <!-- {{ fullname }} <br /> -->
-    <div class="border border-green-400 rounded-lg shadow bg-red-500 h-full">
-        <img :src="faker.image.urlLoremFlickr({category: 'selfie'})" class="rounded-t-xl" />
-        <div class="p-7">
-            <p class="mb-2 text-3xl font-serif">{{ fullname }}</p>
-            <p class="mb-3 font-sans text-blue-800">{{ job_title }}</p>
-            <p class="mb-3 font-sans text-blue-800">{{ bio }}</p>
+    <RouterLink :to="{name: 'CardDetails', params: {id: username}}">
+        <div class="border border-green-400 rounded-lg shadow bg-red-500 h-full">
+            <img :src="faker.image.urlLoremFlickr({category: 'selfie'})" class="rounded-t-xl" />
+            <div class="p-7">
+                <p class="mb-2 text-3xl font-serif">{{ fullname }}</p>
+                <p class="mb-3 font-sans text-blue-800">{{ job_title }}</p>
+                <p class="mb-3 font-sans text-blue-800">{{ bio }}</p>
+            </div>
         </div>
-    </div>
+    </RouterLink>
 </template>
